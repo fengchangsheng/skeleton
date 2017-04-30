fd67dcdcd1b7c0cf585168fe022d94916872041e
lucare <fengchangsheng123@qq.com>
2017-04-30 22:54:55 +0800

    字符串格式更新

--- src/test/resources/skeleton_shiro.sql
+++ src/test/resources/skeleton_shiro.sql
@@ -21,7 +21,7 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `t_permission`;
 CREATE TABLE `t_permission` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `type` tinyint(2) DEFAULT NULL COMMENT ''权限类型'',
+  `type` tinyint(2) DEFAULT NULL COMMENT "权限类型",
   `name` varchar(255) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `permission` varchar(255) DEFAULT NULL,
@@ -31,11 +31,11 @@ CREATE TABLE `t_permission` (
 -- ----------------------------
 -- Records of t_permission
 -- ----------------------------
-INSERT INTO `t_permission` VALUES (''1'', null, ''用户创建'', null, ''admin:user:create'');
-INSERT INTO `t_permission` VALUES (''2'', null, ''用户查看'', null, ''admin:user:view'');
-INSERT INTO `t_permission` VALUES (''3'', null, ''用户删除'', null, ''admin:user:delete'');
-INSERT INTO `t_permission` VALUES (''4'', null, ''用户更新'', null, ''admin:user:update'');
-INSERT INTO `t_permission` VALUES (''5'', null, ''角色查看'', null, ''admin:role:view'');
+INSERT INTO `t_permission` VALUES ("1", null, "用户创建", null, "admin:user:create");
+INSERT INTO `t_permission` VALUES ("2", null, "用户查看", null, "admin:user:view");
+INSERT INTO `t_permission` VALUES ("3", null, "用户删除", null, "admin:user:delete");
+INSERT INTO `t_permission` VALUES ("4", null, "用户更新", null, "admin:user:update");
+INSERT INTO `t_permission` VALUES ("5", null, "角色查看", null, "admin:role:view");
 
 -- ----------------------------
 -- Table structure for t_role
@@ -51,8 +51,8 @@ CREATE TABLE `t_role` (
 -- ----------------------------
 -- Records of t_role
 -- ----------------------------
-INSERT INTO `t_role` VALUES (''1'', ''admin'', ''管理员'');
-INSERT INTO `t_role` VALUES (''2'', ''leader'', ''公司领导'');
+INSERT INTO `t_role` VALUES ("1", "admin", "管理员");
+INSERT INTO `t_role` VALUES ("2", "leader", "公司领导");
 
 -- ----------------------------
 -- Table structure for t_role_permission
@@ -68,12 +68,12 @@ CREATE TABLE `t_role_permission` (
 -- ----------------------------
 -- Records of t_role_permission
 -- ----------------------------
-INSERT INTO `t_role_permission` VALUES (''1'', ''1'', ''1'');
-INSERT INTO `t_role_permission` VALUES (''2'', ''1'', ''2'');
-INSERT INTO `t_role_permission` VALUES (''3'', ''1'', ''3'');
-INSERT INTO `t_role_permission` VALUES (''4'', ''1'', ''4'');
-INSERT INTO `t_role_permission` VALUES (''5'', ''2'', ''1'');
-INSERT INTO `t_role_permission` VALUES (''6'', ''2'', ''5'');
+INSERT INTO `t_role_permission` VALUES ("1", "1", "1");
+INSERT INTO `t_role_permission` VALUES ("2", "1", "2");
+INSERT INTO `t_role_permission` VALUES ("3", "1", "3");
+INSERT INTO `t_role_permission` VALUES ("4", "1", "4");
+INSERT INTO `t_role_permission` VALUES ("5", "2", "1");
+INSERT INTO `t_role_permission` VALUES ("6", "2", "5");
 
 -- ----------------------------
 -- Table structure for t_user
@@ -84,25 +84,25 @@ CREATE TABLE `t_user` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
-  `state` tinyint(2) DEFAULT ''1'' COMMENT ''启用/禁用'',
+  `state` tinyint(2) DEFAULT "1" COMMENT "启用/禁用",
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=839662116311633921 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_user
 -- ----------------------------
-INSERT INTO `t_user` VALUES (''1'', ''三毛'', null, ''15669852653'', ''1'');
-INSERT INTO `t_user` VALUES (''836043268161261568'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''837134894975549440'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''837135134361255936'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''837135380675952640'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''837135380675952641'', ''fcs'', ''123'', null, ''1'');
-INSERT INTO `t_user` VALUES (''839659443315163136'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''839660000863993856'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''839660220125437952'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''839660221983514624'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''839661736089612288'', ''张三'', ''456'', ''15526538456'', ''1'');
-INSERT INTO `t_user` VALUES (''839662116311633920'', ''张三'', ''456'', ''15526538456'', ''1'');
+INSERT INTO `t_user` VALUES ("1", "三毛", null, "15669852653", "1");
+INSERT INTO `t_user` VALUES ("836043268161261568", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("837134894975549440", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("837135134361255936", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("837135380675952640", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("837135380675952641", "fcs", "123", null, "1");
+INSERT INTO `t_user` VALUES ("839659443315163136", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("839660000863993856", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("839660220125437952", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("839660221983514624", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("839661736089612288", "张三", "456", "15526538456", "1");
+INSERT INTO `t_user` VALUES ("839662116311633920", "张三", "456", "15526538456", "1");
 
 -- ----------------------------
 -- Table structure for t_user_role
@@ -118,5 +118,5 @@ CREATE TABLE `t_user_role` (
 -- ----------------------------
 -- Records of t_user_role
 -- ----------------------------
-INSERT INTO `t_user_role` VALUES (''1'', ''837135380675952641'', ''1'');
-INSERT INTO `t_user_role` VALUES (''2'', ''837135380675952641'', ''2'');
+INSERT INTO `t_user_role` VALUES ("1", "837135380675952641", "1");
+INSERT INTO `t_user_role` VALUES ("2", "837135380675952641", "2");
