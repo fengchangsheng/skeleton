098f497c8485e974f0f134a07d6b00120acdced5
lucare <fengchangsheng123@qq.com>
2018-01-28 21:14:59 +0800

    mp升级到2.1.8

--- src/main/java/com/fcs/common/config/WebConfig.java
+++ src/main/java/com/fcs/common/config/WebConfig.java
@@ -1,10 +1,15 @@
 package com.fcs.common.config;
 
-import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -17,12 +22,22 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-        FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fjc = new FastJsonConfig();
-        //1、序列化重点
-        fjc.setSerializerFeatures(SerializerFeature.BrowserCompatible);
-        fastJsonConverter.setFastJsonConfig(fjc);
-        converters.add(fastJsonConverter);
+//        FastJsonHttpMessageConverter fastJsonConverter = new FastJsonHttpMessageConverter();
+//        FastJsonConfig fjc = new FastJsonConfig();
+//        //1、序列化重点
+//        fjc.setSerializerFeatures(SerializerFeature.BrowserCompatible);
+//        fastJsonConverter.setFastJsonConfig(fjc);
+//        converters.add(fastJsonConverter);
+
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        ObjectMapper objectMapper = builder.build();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        objectMapper.configure(MapperFeature.PROPAGATE_TRANSIENT_MARKER, true);// 忽略 transient 修饰的属性
+        converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+        super.configureMessageConverters(converters);
     }
 
 }
